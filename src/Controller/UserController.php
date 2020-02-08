208e698ca50daa4619b27ec4da5d4b3cfba6bab4
Weder77 <dany.jc@yahoo.com>
2020-02-08 17:32:30 +0100

    :sparkles: Add error message at login and constraints at register

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterFormType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
@@ -22,9 +24,17 @@ class UserController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('user/login.html.twig', array());
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 
     /**
--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -3,27 +3,59 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegisterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('username', TextType::class)
-        ->add('password', PasswordType::class)
-        ->add('email', EmailType::class)
-        ->add('file', FileType::class, array(
-            'required' => false
+        ->add('username', TextType::class, array(
+            'constraints' => array(
+                new Assert\NotBlank(array(
+                    'message' => 'This field is required'
+                )),
+                new Assert\Length(array(
+                    'min' => 2,
+                    'max' => 180,
+                    'minMessage' => 'Your username must be more than 2 letters.',
+                    'maxMessage' => 'Your username must be less than 180 letters. '
+                ))
+                ),
         ))
+
+        ->add('password', PasswordType::class, array(
+            'constraints' => array(
+                new Assert\NotBlank(array(
+                    'message' => 'This field is required'
+                )),
+                new Assert\Length(array(
+                    'min' => 2,
+                    'max' => 180,
+                    'minMessage' => 'Your password must be more than 2 letters.',
+                    'maxMessage' => 'Your password must be less than 180 letters. '
+                ))
+                ),
+        ))
+
+
+        ->add('email', EmailType::class)
+
+
+        ->add('file', FileType::class, array(
+            'required' => false,
+        ))
+        
+    
         ->add('Register', SubmitType::class, [
             'attr' => ['class' => 'btn btn-custom'],
         ]);
