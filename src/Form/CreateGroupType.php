89970563115919d081ea2cc575a42377d95f7c60
Weder77 <dany.jc@yahoo.com>
2020-02-08 18:07:53 +0100

    :dizzy: add mdoal to create group

--- src/Form/CreateGroupType.php
+++ src/Form/CreateGroupType.php
@@ -8,7 +8,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CreateGroupType extends AbstractType
@@ -16,7 +18,19 @@ class CreateGroupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+        ->add('name', TextType::class, array(
+            'constraints' => array(
+                new Assert\NotBlank(array(
+                    'message' => 'This field is required'
+                )),
+                new Assert\Length(array(
+                    'min' => 2,
+                    'max' => 255,
+                    'minMessage' => 'Your group name must be more than 2 letters.',
+                    'maxMessage' => 'Your group na must be less than 255 letters. '
+                ))
+                ),
+        ))
             ->add('file', FileType::class, array(
                 'required' => false
             ))
@@ -26,11 +40,9 @@ class CreateGroupType extends AbstractType
                 'multiple' => true,
                 'expanded' => true,
                 'choice_label' => 'username',
-            ))
+            ));
             // ->add('users_admin')
-            ->add('Create Group', SubmitType::class, [
-                'attr' => ['class' => 'btn btn-light'],
-            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
